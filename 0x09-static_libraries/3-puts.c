a528d09a99928f9196898cb73bd78db68cb8e91c
JayRolla <molapokgarose@gmail.com>
2023-10-27 07:49:12 -0700

    static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h> // Include <stdio.h> here
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
